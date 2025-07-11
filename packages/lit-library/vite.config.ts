c75e3a573bd99b1963213617b2b7225a6466e37f
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-07-11 13:50:51 +0200

    stencil bundle stats

--- packages/lit-library/vite.config.ts
+++ packages/lit-library/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer({
-      open: true,
       filename: 'bundle-stats.html',
       gzipSize: true
     })
--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -1,4 +1,7 @@
 import { Config } from '@stencil/core';
+import alias from '@rollup/plugin-alias';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { resolve } from 'path';
 
 export const config: Config = {
   namespace: 'stencil-library',
@@ -11,7 +14,22 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
-  testing: {
-    browserHeadless: "shell",
+  rollupPlugins: {
+    before: [
+      alias({
+        entries: [
+          {
+            find: '@git-diff-view/lowlight',
+            replacement: resolve(__dirname, 'src/utils/lowlight/index.ts'),
+          },
+        ],
+      }),
+    ],
+    after: [
+      visualizer({
+        filename: 'bundle-stats.html',
+        gzipSize: true
+      }),
+    ],
   },
 };
